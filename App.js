4308b22c4b7bdadf4139710d606907483bd6e036
Wassimsghaier420 <52701776+Wassimsghaier420@users.noreply.github.com>
2019-08-16 14:43:08 +0100

    fifth commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="time">
+    <h1>
+      00:30:00
+    </h1>
+      </div>
+      <div className="unit">
+      <p>
+        Hour Minute Second
+      </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
